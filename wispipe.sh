ee2bdab7672ad9ea9efe1334d2e19bbc52aec320
Marc Rafelski <mrafelski@stsci.edu>
2016-10-04 09:38:57 -0400

    Modified wispipe.sh and wispipe_uvis.sh to no longer require the directories to be specified. If now just assumes $WISPDATA and $WISPIPE, and just wants you tog ive it the Par#.

--- wispipe.sh
+++ wispipe.sh
@@ -9,22 +9,22 @@
 # use findf.pro to check Q/A of flt before the pipeline
 # use qacheck.pro to check Q/A of drz.fits after the pipeline
 # use mastreadme.pro to prepare the tar file for mast delivery
-# Call in csh:  ./wispipe.sh Par# /Volumes/Kudo/DATA/WISPS ~/WISPIPE > & log#-[datetime].log
+# Call in csh:  ./wispipe.sh Par# > & log#-[datetime].log
 ###############################################################    
 #source ~/.cshrc
 
 
 
 ur_setup
-cd $3/IDL/
+cd $WISPIPE/IDL/
 
 
 idl<< EOF
 .run process_IB2.pro
-process_IB2, "$1","$2","$3"
+process_IB2, "$1","$WISPDATA","$WISPIPE"
 .run cross_clean.pro
 .run im_clean_IB6.pro
-im_clean_IB6,"$1",/both,"$2","$3"
+im_clean_IB6,"$1",/both,"$WISPDATA","$WISPIPE"
 EOF
 
 
@@ -32,14 +32,14 @@ EOF
 # Before everything, wcs are updated on grism and direct
 # exposures.
 ##########################################################################
-cd $2/aXe/$1/
+cd $WISPDATA/aXe/$1/
 python IR_updatewcs.py
 ##########################################################################
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .r tweakprep_IB1.pro
-tweakprep_IB1,"$1","$2"
+tweakprep_IB1,"$1","$WISPDATA"
 EOF
 
 ##########################################################################
@@ -49,39 +49,39 @@ EOF
 # created but the wcs of grism and direct exposures are still
 # the original ones  
 ##########################################################################
-cd $2/aXe/$1/
-# cp $3/aXe/fit_multi_sky.py .
-cp $3/PYTHON/fit_multi_sky.py .
+cd $WISPDATA/aXe/$1/
+# cp $WISPIPE/aXe/fit_multi_sky.py .
+cp $WISPIPE/PYTHON/fit_multi_sky.py .
 python fit_multi_sky.py "$WISPIPE/aXe/CONFIG/grism_master_sky_v0.5/"
 ##########################################################################
 
 # -------  A  ---------
  
-cd $2/aXe/$1/DATA/DIRECT/
+cd $WISPDATA/aXe/$1/DATA/DIRECT/
 python tweakprep.py
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .r tweaksex_IB7.pro
-tweaksex_IB7,"$1","$2","$3"
+tweaksex_IB7,"$1","$WISPDATA","$WISPIPE"
 EOF
-cd $2/aXe/$1/DATA/DIRECT/
+cd $WISPDATA/aXe/$1/DATA/DIRECT/
 python tweakreg.py
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .r tweakprepgrism_IB6.pro
-tweakprepgrism_IB6,"$1","$2","$3"
+tweakprepgrism_IB6,"$1","$WISPDATA","$WISPIPE"
 EOF
 
 
-cd $2/aXe/$1/DATA/GRISM/
+cd $WISPDATA/aXe/$1/DATA/GRISM/
 python tweakprepgrism.py
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .r new_drizprep_IB2.pro
-new_drizprep_IB2,"$1","$2"
+new_drizprep_IB2,"$1","$WISPDATA"
 EOF
 
 # -------  B  ---------
@@ -92,25 +92,25 @@ EOF
  # GRISM folders ONLY
 ####################################################################################       
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .run smooth_and_combine3.pro
-smooth_and_combine3,"$1","$2"
+smooth_and_combine3,"$1","$WISPDATA"
 EOF
-cd $2/aXe/$1/DATA/DIRECT_GRISM/
+cd $WISPDATA/aXe/$1/DATA/DIRECT_GRISM/
 python smooth_and_combine.py
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .run new_depth.pro
 .run new_match_cat3.pro
 .run new_seflag.pro
-new_match_cat3,"$1","$2"
+new_match_cat3,"$1","$WISPDATA"
 EOF
 
 idl<< EOF
 .run ddread.pro
 .run axeprep.pro
-axeprep,"$1",/both,"$2"
+axeprep,"$1",/both,"$WISPDATA"
 EOF
 
 ###########             # -------  C  ---------
@@ -118,19 +118,19 @@ EOF
 ##########################################################################
 # aXe codes for stamps extraction
 ##########################################################################
-cd $2/aXe/$1
+cd $WISPDATA/aXe/$1
 python G102_axe_V6_1.py
 python G141_axe_V6_1.py
 ##########################################################################
 
 # -------  D  ---------
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .run mk_region_direct_IB1.pro
-mk_region_direct_IB1,"$1","$2"
+mk_region_direct_IB1,"$1","$WISPDATA"
 .r rename_cat
-rename_cat,"$1","$2"
+rename_cat,"$1","$WISPDATA"
 EOF
 
 ##########################################################################
@@ -138,14 +138,14 @@ EOF
 # images are created here.
 # Written by Vihang Mehta
 ##########################################################################
-cd $2/aXe/$1/
-cp $3/PYTHON/WISP_grism_region_files.py .
+cd $WISPDATA/aXe/$1/
+cp $WISPIPE/PYTHON/WISP_grism_region_files.py .
 python WISP_grism_region_files.py G102 -c "$WISPIPE/aXe/CONFIG/"
 python WISP_grism_region_files.py G141 -c "$WISPIPE/aXe/CONFIG/"
 #usage: WISP_grism_region_files.py [-h] [-f FILTER] [-c CONFIG] [-p PAR_DIR] grism
 ##########################################################################
 
-cd $2/aXe/$1
+cd $WISPDATA/aXe/$1
 
 # -------  E  ---------
 
@@ -154,23 +154,23 @@ cd $2/aXe/$1
 # script finds make_pdf.sh, the error will not interrupt the reduction
 # of other Par fields when they run in "make_pdf.sh"
 # #########################################################################
-cd $2/aXe/$1/Plots
-cp $3/make_pdf.sh . # Fake program to prevent interruptions in case of errors
+cd $WISPDATA/aXe/$1/Plots
+cp $WISPIPE/make_pdf.sh . # Fake program to prevent interruptions in case of errors
 ##########################################################################
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .r beam_pet.pro 
 .r axe_sing_IB1.pro
 .r wisp_extract_IB1.pro
-wisp_extract_IB1,"$1",0,"$2"
+wisp_extract_IB1,"$1",0,"$WISPDATA"
 EOF
 
 
 ##########################################################################
 # making single pdf file
 ##########################################################################
-cd $2/aXe/$1/Plots
+cd $WISPDATA/aXe/$1/Plots
 source make_pdf.sh # written in wisp_extract.pro
 rm spectra*.pdf
 ##########################################################################
@@ -181,15 +181,15 @@ rm spectra*.pdf
 # it modifies the files.dat to update the contamination column.
 # written by Hugh Dickinson
 ##########################################################################
-cd $2/aXe/$1/
-cp $3/PYTHON/ProcessZerothOrders.py .
-cp $3/PYTHON/ZerothOrderWavelengthRanges.py .
+cd $WISPDATA/aXe/$1/
+cp $WISPIPE/PYTHON/ProcessZerothOrders.py .
+cp $WISPIPE/PYTHON/ZerothOrderWavelengthRanges.py .
 python ProcessZerothOrders.py --verbose # --dryrun
 ##########################################################################
 
 # -------  F  ---------
 
-cd $2/aXe/$1
+cd $WISPDATA/aXe/$1
 #tar -cvf "$1"_final_V5.0.tar   Spectra 	Stamps  DATA/DIRECT_GRISM/fin_F*.cat  DATA/DIRECT_GRISM/*.reg  DATA/DIRECT_GRISM/F*.fits DATA/DIRECT_GRISM/G*.fits SEX/F*full.cat SEX/cat_deblend_flag.cat Plots/*.pdf  G102_DRIZZLE G141_DRIZZLE 
 #tar -cvf "$1"_final_V5.1.tar Spectra  Stamps Plots/*.pdf G102_DRIZZLE G141_DRIZZLE DATA/DIRECT_GRISM/F*_drz.fits DATA/DIRECT_GRISM/fin_*cat DATA/DIRECT_GRISM/*_flag.cat DATA/DIRECT_GRISM/G*.fits DATA/DIRECT_GRISM/*reg DATA/DIRECT_GRISM/i*1.cat DATA/DIRECT_GRISM/F1*.fits DATA/DIRECT/F*_drz.fits DATA/DIRECT/F*drz*.fits DATA/GRISM/G*drz.fits DATA/GRISM/G*drz*.fits
 tar -cvf "$1"_final_V6.1.tar Spectra Stamps DATA/DIRECT_GRISM/fin_F*.cat DATA/DIRECT_GRISM/*.reg DATA/DIRECT_GRISM/F*drz.fits DATA/DIRECT_GRISM/F*sci.fits DATA/DIRECT_GRISM/F*rms.fits DATA/DIRECT_GRISM/F*wht.fits DATA/DIRECT_GRISM/F*0.fits DATA/DIRECT_GRISM/JH*.fits DATA/DIRECT_GRISM/G*.fits SEX/F*full.cat Plots/*.pdf G102_DRIZZLE G141_DRIZZLE # SEX/cat_deblend_flag.cat DATA/DIRECT_GRISM/*_flag.cat DATA/UVIS/F*.fits DATA/UVIS/UVIStoIR DATA/UVIS/IRtoUVIS 
--- wispipe_uvis.sh
+++ wispipe_uvis.sh
@@ -7,21 +7,22 @@
 # Modified by Sophia Dai 2014,2015
 # Modified by Ivano Baronchelli, 2016
 # Run when UVIS data is present instead of wispipe.sh
-# Call in csh:  ./wispipe_uvis.sh Par# /Volumes/Kudo/DATA/WISPS ~/WISPIPE > & log#-[datetime].log
+# use findf.pro to check Q/A of flt before the pipeline
+# use qacheck.pro to check Q/A of drz.fits after the pipeline
+# use mastreadme.pro to prepare the tar file for mast delivery
+# Call in csh:  ./wispipe_uvis.sh Par# > & log#-[datetime].log
 ###############################################################
 
-
-
 ur_setup common primary
-cd $3/IDL/
+cd $WISPIPE/IDL/
 
  
 idl<< EOF
 .run process_IB2.pro
-process_IB2, "$1",/uvis,"$2","$3"
+process_IB2, "$1",/uvis,"$WISPDATA","$WISPIPE"
 .run cross_clean.pro
 .run im_clean_IB6.pro
-im_clean_IB6,"$1",/both,/uvis,"$2","$3"
+im_clean_IB6,"$1",/both,/uvis,"$WISPDATA","$WISPIPE"
 EOF
 
 
@@ -29,15 +30,15 @@ EOF
 # Before everything, wcs are updated on grism and direct
 # exposures. 
 ##########################################################################
-cd $2/aXe/$1/
+cd $WISPDATA/aXe/$1/
 python IR_updatewcs.py
 ##########################################################################
 
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .r tweakprep_IB1.pro
-tweakprep_IB1,"$1","$2"
+tweakprep_IB1,"$1","$WISPDATA"
 EOF
 
 ##########################################################################
@@ -47,38 +48,38 @@ EOF
 # created but the wcs of grism and direct exposures are still
 # the original ones  
 ##########################################################################
-cd $2/aXe/$1/
-# cp $3/aXe/fit_multi_sky.py .
-cp $3/PYTHON/fit_multi_sky.py .
+cd $WISPDATA/aXe/$1/
+# cp $WISPIPE/aXe/fit_multi_sky.py .
+cp $WISPIPE/PYTHON/fit_multi_sky.py .
 python fit_multi_sky.py "$WISPIPE/aXe/CONFIG/grism_master_sky_v0.5/"
 ##########################################################################
 # sky subtraction - END
 ##########################################################################
 
-cd $2/aXe/$1/DATA/DIRECT/
+cd $WISPDATA/aXe/$1/DATA/DIRECT/
 python tweakprep.py
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .r tweaksex_IB7.pro
-tweaksex_IB7,"$1","$2","$3"
+tweaksex_IB7,"$1","$WISPDATA","$WISPIPE"
 EOF
-cd $2/aXe/$1/DATA/DIRECT/
+cd $WISPDATA/aXe/$1/DATA/DIRECT/
 python tweakreg.py
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .r tweakprepgrism_IB6.pro
-tweakprepgrism_IB6,"$1","$2","$3"
+tweakprepgrism_IB6,"$1","$WISPDATA","$WISPIPE"
 EOF
 
-cd $2/aXe/$1/DATA/GRISM/
+cd $WISPDATA/aXe/$1/DATA/GRISM/
 python tweakprepgrism.py
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .r new_drizprep_IB2.pro
-new_drizprep_IB2,"$1","$2"
+new_drizprep_IB2,"$1","$WISPDATA"
 EOF
 
 # -------  A  ---------
@@ -87,32 +88,32 @@ EOF
 ##########################################################################
 ## UVIS SPECIFIC CODES
 ##########################################################################
-cd $2/aXe/$1/DATA/DIRECT_GRISM/
+cd $WISPDATA/aXe/$1/DATA/DIRECT_GRISM/
 python driz.py      
-cd $2/aXe/$1/DATA/UVIS/
+cd $WISPDATA/aXe/$1/DATA/UVIS/
 python uvis_updatewcs.py
 # ////////////////////////////////////
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .run makeorder_A_IB2.pro
-makeorder_A_IB2,"$1","$2"
+makeorder_A_IB2,"$1","$WISPDATA"
 EOF
-cd $2/aXe/$1/DATA/UVIS/
+cd $WISPDATA/aXe/$1/DATA/UVIS/
 python uvis_initial_driz.py # makes a temporary drz image (one per filter)
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .run uvis_initial_twk_IB2.pro
-uvis_initial_twk_IB2,"$1","$2","$3"
+uvis_initial_twk_IB2,"$1","$WISPDATA","$WISPIPE"
 EOF
-cd $2/aXe/$1/DATA/UVIS/
+cd $WISPDATA/aXe/$1/DATA/UVIS/
 python uvis_initial_twk.py
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .run makeorder_B_IB2.pro
-makeorder_B_IB2,"$1","$2"
+makeorder_B_IB2,"$1","$WISPDATA"
 EOF
 # ////////////////////////////////////
-cd $2/aXe/$1/DATA/UVIS/
+cd $WISPDATA/aXe/$1/DATA/UVIS/
 python uvis_driz.py
 ##########################################################################
 ## UVIS SPECIFIC CODES END
@@ -128,25 +129,25 @@ python uvis_driz.py
  # GRISM folders ONLY
 ####################################################################################
 
- cd $3/IDL/
+ cd $WISPIPE/IDL/
 idl<< EOF
 .run smooth_and_combine3.pro
-smooth_and_combine3,"$1","$2"
+smooth_and_combine3,"$1","$WISPDATA"
 EOF
-cd $2/aXe/$1/DATA/DIRECT_GRISM/
+cd $WISPDATA/aXe/$1/DATA/DIRECT_GRISM/
 python smooth_and_combine.py
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .run new_depth.pro
 .run new_match_cat3.pro
 .run new_seflag.pro
-new_match_cat3,"$1","$2"
+new_match_cat3,"$1","$WISPDATA"
 EOF
 
 idl<< EOF
 .run ddread.pro
 .run axeprep.pro
-axeprep,"$1",/both,"$2"
+axeprep,"$1",/both,"$WISPDATA"
 EOF
 
 # -------  C  ---------
@@ -154,19 +155,19 @@ EOF
 ##########################################################################
 # aXe codes for stamps extraction
 ##########################################################################
-cd $2/aXe/$1
+cd $WISPDATA/aXe/$1
 python G102_axe_V6_1.py # (update_wcs=no in last grism astrodrizzle AND no Background subtraction, no grism drizzle, new config)
 python G141_axe_V6_1.py # (update_wcs=no in last grism astrodrizzle AND no Background subtraction, no grism drizzle, new config files)
 ##########################################################################
 
 # -------  D  ---------
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .run mk_region_direct_IB1.pro
-mk_region_direct_IB1,"$1","$2"
+mk_region_direct_IB1,"$1","$WISPDATA"
 .r rename_cat.pro
-rename_cat,"$1","$2"
+rename_cat,"$1","$WISPDATA"
 EOF
 
 
@@ -175,8 +176,8 @@ EOF
 # images are created here.
 # Written by Vihang Mehta
 ##########################################################################
-cd $2/aXe/$1/
-cp $3/PYTHON/WISP_grism_region_files.py .
+cd $WISPDATA/aXe/$1/
+cp $WISPIPE/PYTHON/WISP_grism_region_files.py .
 python WISP_grism_region_files.py G102 -c "$WISPIPE/aXe/CONFIG/"
 python WISP_grism_region_files.py G141 -c "$WISPIPE/aXe/CONFIG/"
 #usage: WISP_grism_region_files.py [-h] [-f FILTER] [-c CONFIG] [-p PAR_DIR] grism
@@ -189,22 +190,22 @@ python WISP_grism_region_files.py G141 -c "$WISPIPE/aXe/CONFIG/"
 # script finds make_pdf.sh, the error will not interrupt the reduction
 # of other Par fields when they run in "make_pdf.sh"
 # #########################################################################
-cd $2/aXe/$1/Plots
-cp $3/make_pdf.sh . # Fake program to prevent interruptions in case of errors
+cd $WISPDATA/aXe/$1/Plots
+cp $WISPIPE/make_pdf.sh . # Fake program to prevent interruptions in case of errors
 ##########################################################################
 
-cd $3/IDL/
+cd $WISPIPE/IDL/
 idl<< EOF
 .r beam_pet.pro 
 .r axe_sing_IB1.pro
 .r wisp_extract_IB1.pro
-wisp_extract_IB1,"$1",0,"$2"
+wisp_extract_IB1,"$1",0,"$WISPDATA"
 EOF
 
 ##########################################################################
 # making single pdf file
 ##########################################################################
-cd $2/aXe/$1/Plots
+cd $WISPDATA/aXe/$1/Plots
 source make_pdf.sh # written in wisp_extract.pro
 rm spectra*.pdf
 ##########################################################################
@@ -216,9 +217,9 @@ rm spectra*.pdf
 # it modifies the files.dat to update the contamination column.
 # written by Hugh Dickinson
 ##########################################################################
-cd $2/aXe/$1/
-cp $3/PYTHON/ProcessZerothOrders.py .
-cp $3/PYTHON/ZerothOrderWavelengthRanges.py .
+cd $WISPDATA/aXe/$1/
+cp $WISPIPE/PYTHON/ProcessZerothOrders.py .
+cp $WISPIPE/PYTHON/ZerothOrderWavelengthRanges.py .
 python ProcessZerothOrders.py --verbose # --dryrun
 ##########################################################################
 
@@ -226,14 +227,14 @@ python ProcessZerothOrders.py --verbose # --dryrun
 ##########################################################################
 # making uvis helpfile
 ##########################################################################
-cd $2/aXe/$1/DATA/UVIS
-cp $3/PYTHON/make_uvis_helpfile_IB1.py .
+cd $WISPDATA/aXe/$1/DATA/UVIS
+cp $WISPIPE/PYTHON/make_uvis_helpfile_IB1.py .
 python make_uvis_helpfile_IB1.py
 ##########################################################################
  
 # -------  G  ---------
 
-cd $2/aXe/$1
+cd $WISPDATA/aXe/$1
 #tar -cvf "$1"_final_V5.1.tar Spectra Stamps DATA/DIRECT_GRISM/fin_F*.cat DATA/DIRECT_GRISM/*_flag.cat DATA/DIRECT_GRISM/*.reg DATA/DIRECT_GRISM/F*.fits DATA/DIRECT_GRISM/G*.fits SEX/F*full.cat SEX/cat_deblend_flag.cat Plots/*.pdf  G102_DRIZZLE G141_DRIZZLE DATA/UVIS/F*.fits DATA/UVIS/UVIStoIR DATA/UVIS/IRtoUVIS
 tar -cvf "$1"_final_V6.1.tar Spectra Stamps DATA/DIRECT_GRISM/fin_F*.cat DATA/DIRECT_GRISM/*.reg DATA/DIRECT_GRISM/F*drz.fits DATA/DIRECT_GRISM/F*sci.fits DATA/DIRECT_GRISM/F*rms.fits DATA/DIRECT_GRISM/F*wht.fits DATA/DIRECT_GRISM/F*0.fits DATA/DIRECT_GRISM/JH*.fits DATA/DIRECT_GRISM/G*.fits SEX/F*full.cat  Plots/*.pdf G102_DRIZZLE G141_DRIZZLE DATA/UVIS/UVIS*.fits DATA/UVIS/UVIS*.list DATA/UVIS/*.txt DATA/UVIS/UVIStoIR DATA/UVIS/IRtoUVIS # DATA/DIRECT_GRISM/*_flag.cat SEX/cat_deblend_flag.cat
 
