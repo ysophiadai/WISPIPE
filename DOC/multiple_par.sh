f87d7b4f876032155bfef5c3cc45c859ee280b5b
Marc Rafelski <mrafelski@stsci.edu>
2016-11-10 11:06:14 -0500

    removed & since running in bash

--- DOC/multiple_par.sh
+++ DOC/multiple_par.sh
@@ -1,15 +1,15 @@
-#!/bin/csh
+#!/bin/bash
 
 ##########################################################################################################################
 # HOW TO LAUNCH MULTIPLE PROCESSES AT THE SAME TIME
 #
 # 1) Cancel all the old processes eventually present (otherwise they would be run )
 # 2) Add two new line with the following sintax:
-#    > cd <path-WISPIPE>/WISPIPE
-#    > source wispipe_6_1.sh Par# <path-of-aXe-and-data-folders> <path-of-wispipe-programs> > & log_Par#_6.1.log
+#    > cd $WISPIPE
+#    > source wispipe.sh Par# > & log_Par#_6.1.log
 #    EXAMPLE:
-#    > cd Users/ivano/WISPIPE
-#    > source wispipe_6_1.sh Par364 /Volumes/PROMISE_PEGASUS/TEST_DIR/DATA/WISPS /Users/ivano/WISPIPE > & log_364_6.1.log
+#    > cd $WISPIPE
+#    > source wispipe.sh Par364 > & log_364_6.1.log
 # 4) Press enter multiple times (approximately 25-30 times for each field processed, also depending on the number of
 #    exposures processed)
 #
@@ -20,21 +20,21 @@
 # Ivano Baronchelli 2016
 ###########################################################################################################################
 
-cd ~/WISPIPE
-source wispipe_6_1.sh Par1 > & LOG/log_1_6.1.log
-cd ~/WISPIPE
-source wispipe_6_1.sh Par3 > & LOG/log_3_6.1.log
-cd ~/WISPIPE		   
-source wispipe_6_1.sh Par4 > & LOG/log_4_6.1.log
-cd ~/WISPIPE		   
-source wispipe_6_1.sh Par5 > & LOG/log_5_6.1.log
-cd ~/WISPIPE		   
-source wispipe_6_1.sh Par6 > & LOG/log_6_6.1.log
-cd ~/WISPIPE		   
-source wispipe_6_1.sh Par7 > & LOG/log_7_6.1.log
-cd ~/WISPIPE		   
-source wispipe_6_1.sh Par8 > & LOG/log_8_6.1.log
-cd ~/WISPIPE		   
-source wispipe_6_1.sh Par9 > & LOG/log_9_6.1.log
-cd ~/WISPIPE
-source wispipe_6_1.sh Par1 > & LOG/log_10_6.1.log
+cd $WISPIPE
+source wispipe.sh Par1 > & LOG/log_1_6.1.log
+cd $WISPIPE
+source wispipe.sh Par3 > & LOG/log_3_6.1.log
+cd $WISPIPE		   
+source wispipe.sh Par4 > & LOG/log_4_6.1.log
+cd $WISPIPE		   
+source wispipe.sh Par5 > & LOG/log_5_6.1.log
+cd $WISPIPE		   
+source wispipe.sh Par6 > & LOG/log_6_6.1.log
+cd $WISPIPE		   
+source wispipe.sh Par7 > & LOG/log_7_6.1.log
+cd $WISPIPE		   
+source wispipe.sh Par8 > & LOG/log_8_6.1.log
+cd $WISPIPE		   
+source wispipe.sh Par9 > & LOG/log_9_6.1.log
+cd $WISPIPE
+source wispipe.sh Par1 > & LOG/log_10_6.1.log
